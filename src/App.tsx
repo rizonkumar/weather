6aa4401b36619173b8ff5e073b1bf21f16fc9bba
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-14 21:44:51 +0530

    Feat: Added API End Points and Types of it

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,32 @@ import Layout from "@/components/layout.tsx";
 import { ThemeProvider } from "@/context/theme-provider.tsx";
 import WeatherDashboard from "@/pages/weather-dashboard.tsx";
 import CityPage from "@/pages/city-page.tsx";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <BrowserRouter>
-        <Layout />
-        <Routes>
-          <Route path="/" element={<WeatherDashboard />} />
-          <Route path="/city/:cityName" element={<CityPage />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <BrowserRouter>
+          <Layout />
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
