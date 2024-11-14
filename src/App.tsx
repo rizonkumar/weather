e863591fbdb4abf82fabe7defba007101bf443d4
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-14 08:50:45 +0530

    Feat: Created Weather Dashboard and City Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "@/components/layout.tsx";
 import { ThemeProvider } from "@/context/theme-provider.tsx";
+import WeatherDashboard from "@/pages/weather-dashboard.tsx";
+import CityPage from "@/pages/city-page.tsx";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
         <Layout />
+        <Routes>
+          <Route path="/" element={<WeatherDashboard />} />
+          <Route path="/city/:cityName" element={<CityPage />} />
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );
---
+++ src/pages/weather-dashboard.tsx
@@ -0,0 +1,5 @@
+function WeatherDashboard() {
+  return <div></div>;
+}
+
+export default WeatherDashboard;
